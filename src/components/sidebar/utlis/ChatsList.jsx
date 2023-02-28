79a432e9f9067a2074ac45ece6b5bad589a23a02
falcon_codes <86522183+Prathamesh-Chavan-232@users.noreply.github.com>
2023-02-28 09:22:26 +0530

    feat: MIXINS

--- src/components/sidebar/utlis/ChatsList.jsx
+++ src/components/sidebar/utlis/ChatsList.jsx
@@ -3,7 +3,7 @@ import { ChatCard } from "./ChatCard";
 
 export const ChatsList = () => {
 	const chatsList = [];
-	for (let i = 0; i < 5; i++) {
+	for (let i = 0; i < 14; i++) {
 		chatsList.push(<ChatCard key={i} />);
 	}
 	return <div className='chats-list'> {chatsList}</div>;
