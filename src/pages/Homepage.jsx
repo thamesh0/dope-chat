35455edec584de7218c23ff05d02e4cf4954b36f
falcon_codes <86522183+Prathamesh-Chavan-232@users.noreply.github.com>
2023-03-08 18:42:48 +0530

    refactor: build for production

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { ChatScreen } from "../components/chat_screen/ChatScreen";
 import { Sidebar } from "../components/sidebar/Sidebar";
 export const Homepage = () => {
-	return (
-		<div className='home_container'>
-			<div className='home'>
-				<Sidebar />
-				<ChatScreen />
-			</div>
-		</div>
-	);
+  return (
+    <div className="home_container">
+      <div className="home">
+        <Sidebar />
+        <ChatScreen />
+      </div>
+    </div>
+  );
 };
