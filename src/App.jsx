c93bd96c457331f4d83d072dfcd2859f74b9d62b
falconcodes <prathamesh.chavanpsc2018@gmail.com>
2024-06-10 13:43:06 +0530

    chore: remove build files

--- src/App.jsx
+++ src/App.jsx
@@ -2,65 +2,39 @@ import "./css/style.scss";
 import { Login } from "./pages/Login";
 import { Signup } from "./pages/Signup";
 import { Homepage } from "./pages/Homepage";
-import {
-	RouterProvider,
-	createBrowserRouter,
-	createRoutesFromElements,
-	BrowserRouter,
-	Routes,
-	Route,
-	Navigate,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthContext } from "./utlis/AuthContext";
 import { useContext } from "react";
 
 function App() {
-	const { currentUser } = useContext(AuthContext);
-	console.log(`current user - ${currentUser}`);
+  const { currentUser } = useContext(AuthContext);
+  console.log(`current user - ${currentUser}`);
 
-	const ProtectedRoute = ({ children }) => {
-		if (!currentUser) {
-			return <Navigate to="/login" />;
-		}
-		return children;
-	};
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+    return children;
+  };
 
-	// const router = createBrowserRouter(
-	// 	createRoutesFromElements(
-	// 		<Route path="/">
-	// 			<Route
-	// 				index
-	// 				element={
-	// 					<ProtectedRoute>
-	// 						<Homepage />
-	// 					</ProtectedRoute>
-	// 				}
-	// 			/>
-	// 			<Route path="login" element={<Login />} />
-	// 			<Route path="signup" element={<Signup />} />
-	// 		</Route>
-	// 	)
-	// );
-
-	return (
-		// <RouterProvider router={router} />
-		<BrowserRouter>
-			<Routes>
-				<Route path="/">
-					<Route
-						index
-						element={
-							<ProtectedRoute>
-								<Homepage />
-							</ProtectedRoute>
-						}
-					/>
-					<Route path="login" element={<Login />} />
-					<Route path="signup" element={<Signup />} />
-				</Route>
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route
+            index
+            element={
+              <ProtectedRoute>
+                <Homepage />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
